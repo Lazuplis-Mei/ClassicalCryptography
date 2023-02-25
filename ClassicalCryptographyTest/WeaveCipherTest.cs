4f6f58f8e4696ab4ed366421132eb8064f25a11d
Lazuplis-Mei <50167216+Lazuplis-Mei@users.noreply.github.com>
2023-02-25 18:10:34 +0800

    WeaveCipher
    I will do it for you!

---
+++ ClassicalCryptographyTest/WeaveCipherTest.cs
@@ -0,0 +1,64 @@
+﻿using ClassicalCryptography.Interfaces;
+using ClassicalCryptography.Replacement;
+using ClassicalCryptography.Undefined;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing.Common;
+
+namespace ClassicalCryptographyTest
+{
+    [TestClass]
+    [SupportedOSPlatform("windows")]
+    public class WeaveCipherTest
+    {
+        [TestMethod]
+        public void TestWeaveCipher()
+        {
+            var bits1 = new BitArray();
+            bits1.appendBit(true);
+            bits1.appendBit(false);
+            bits1.appendBit(false);
+            bits1.appendBit(true);
+            bits1.appendBit(true);
+            bits1.appendBit(false);
+            bits1.appendBit(true);
+            bits1.appendBit(false);
+            bits1.appendBit(true);
+
+            var bits2 = new BitArray();
+            bits2.appendBit(false);
+            bits2.appendBit(false);
+            bits2.appendBit(false);
+            bits2.appendBit(true);
+            bits2.appendBit(true);
+            bits2.appendBit(true);
+            bits2.appendBit(false);
+            bits2.appendBit(true);
+            bits2.appendBit(false);
+
+            var m = WeaveCipher.EncryptBits(bits1, bits2);
+
+            //WeaveCipher.BitsToImage(m, @"E:/test.png");
+            //WeaveCipher.Encrypt("就非常的浪费", @"E:/test.png");
+
+            Debug.WriteLine(m);/*
+  X X X   X X     X 
+    X       X X     
+  X X X   X X     X 
+    X       X X     
+X       X     X X   
+    X       X X     
+X       X     X X   
+X X   X X X     X X 
+  X X X   X X     X 
+    X       X X     
+*/
+        }
+    }
+}
